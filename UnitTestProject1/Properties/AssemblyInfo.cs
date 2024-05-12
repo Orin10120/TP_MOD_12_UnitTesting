475600fff86b61ce00a85781ba42263bb29af3d6
Orin10120 <rindang3001@gmail.com>
2024-05-12 22:53:14 +0700

    Add project files.

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eef775e6-8784-4002-8f41-5ac96afdb1c5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TP_MOD_12_1302223023;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CariTandaBilangan_InputNegatif_ReturnsNegatif()
+        {
+            Form1 form = new Form1();
+            int input = -5;
+            string Eo = "Negatif";
+
+            //act
+            string output = form.CariTandaBilangan(input);
+
+            //ass
+            Assert.AreEqual(Eo, output);
+        }
+
+        [TestMethod]
+        public void CariTandaBilangan_InputPositif_ReturnsPositif()
+        {
+            Form1 form = new Form1();
+            int input = 5;
+            string Eo = "Positif";
+
+            //act
+            string output = form.CariTandaBilangan(input);
+
+            //ass
+            Assert.AreEqual(Eo, output);
+        }
+
+        [TestMethod]
+        public void CariTandaBilangan_InputNol_ReturnsNol()
+        {
+            Form1 form = new Form1();
+            int input = 0;
+            string Eo = "Nol";
+
+            //act
+            string output = form.CariTandaBilangan(input);
+
+            //ass
+            Assert.AreEqual(Eo, output);
+        }
+    }
+}
